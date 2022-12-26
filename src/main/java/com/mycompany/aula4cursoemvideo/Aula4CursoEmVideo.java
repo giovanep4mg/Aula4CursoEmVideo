9ca84761a4caea20066ccfbbf4c3faed6652732c
giovani <115195600+giovanep4mg@users.noreply.github.com>
2022-12-26 08:33:57 -0300

    first commit

---
+++ src/main/java/com/mycompany/aula4cursoemvideo/Aula4CursoEmVideo.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.aula4cursoemvideo;
+
+public class Aula4CursoEmVideo {
+
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta("bic", 0.05f, true, "azul");
+         
+       c1.status();
+        
+       Caneta c2 = new Caneta("nego", 1.0f, false, "preta");
+       
+       c2.status();
+        
+        
+        
+       
+    }
+}
